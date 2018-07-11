ba51348e89f2b15102936e18d2458d104fad25ec
josephiznot <josephiznot@gmail.com>
2018-07-11 10:05:42 -0500

    tracking traffic

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const restCtrls = require("./Controllers/restCtrls");
 const session = require("express-session");
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/../build`));
+// app.use(express.static(`${__dirname}/../build`));
+
 app.use(
   session({
     resave: true,
@@ -17,26 +18,26 @@ app.use(
     }
   })
 );
+
 app.use((req, res, next) => {
   if (!req.session.traffic) {
     req.session.traffic = {};
   }
-  if (!req.session.traffic[req.method + req._parsedOriginalUrl.pathname]) {
-    req.session.traffic[req.method + req._parsedOriginalUrl.pathname] = {};
+  if (!req.session.traffic[req.method + req.originalUrl]) {
+    req.session.traffic[req.method + req.originalUrl] = {};
   }
+
   next();
 });
 
 app.use((req, res, next) => {
-  let views =
-    req.session.traffic[req.method + req._parsedOriginalUrl.pathname].views ||
-    0;
-  req.session.traffic[req.method + req._parsedOriginalUrl.pathname] = {
+  let views = req.session.traffic[req.method + req.originalUrl].views || 0;
+  req.session.traffic[req.method + req.originalUrl] = {
     method: req.method,
     views: views + 1
   };
-  console.log(req.session.traffic);
   next();
+  console.log(req.session.traffic);
 });
 
 app.get("/api/crimes", restCtrls.gitter);
@@ -44,10 +45,10 @@ app.put("/api/crimes/:id", restCtrls.putter);
 app.post("/api/crimes", restCtrls.poster);
 app.delete("/api/crimes/:id", restCtrls.deleter);
 
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
+// const path = require("path");
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
 
 app.listen(port, function() {
   console.log(`I am listening at port ${port}`);
